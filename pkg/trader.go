a881e18ca1682c300d75c03f17cbee36c1372608
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-12 01:56:33 +0330

    adding ring mask to output

--- pkg/trader.go
+++ pkg/trader.go
@@ -8,9 +8,10 @@ import (
 )
 
 type Data struct {
-	Traders  map[string]Trader
-	Coins    map[string]CoinTable
-	RunCoins [][]string
+	Traders     map[string]Trader
+	Coins       map[string]CoinTable
+	RunCoins    [][]string
+	CoinCounter uint
 }
 
 type Trader struct {
@@ -20,9 +21,25 @@ type Trader struct {
 	Data    *Data   `json:"-"`
 }
 
+func CreateTrader(account float64, wallet string, coinTypeCount int) *Trader {
+	return &Trader{
+		ID:      tools.SHA256str(wallet + "-" + strconv.Itoa(int(coinTypeCount))),
+		Account: account,
+		Wallet:  wallet,
+		Data: &Data{
+			CoinCounter: 0,
+			Traders:     make(map[string]Trader),
+			Coins:       make(map[string]CoinTable),
+			RunCoins:    make([][]string, coinTypeCount+1),
+		},
+	}
+}
+
 func (t *Trader) CreateCoin(amount float64, coinType uint) *CoinTable {
+	t.Data.CoinCounter++
+
 	return &CoinTable{
-		ID:       tools.SHA256str(t.ID + "-" + strconv.Itoa(int(coinType))),
+		ID:       tools.SHA256str(t.ID + "-" + strconv.Itoa(int(coinType)) + "-" + strconv.Itoa(int(t.Data.CoinCounter))),
 		Amount:   amount,
 		Status:   Run,
 		Type:     coinType,
@@ -57,10 +74,10 @@ func (t *Trader) checkForCooperationRings() *CooperationTable {
 		}
 	}
 
-	selectedRing, weight := t.selectRing()
+	ringMask, selectedRing, weight := t.selectRing()
 
 	return &CooperationTable{
-		ID:          tools.SHA256str(fmt.Sprint(selectedRing)),
+		ID:          tools.SHA256str(ringMask),
 		MemberCount: uint(len(selectedRing)),
 		Weight:      weight,
 		Investor:    selectedRing[0],
@@ -68,7 +85,7 @@ func (t *Trader) checkForCooperationRings() *CooperationTable {
 	}
 }
 
-func (t *Trader) selectRing() (selectedRing []string, weight float64) {
+func (t *Trader) selectRing() (ringMask string, selectedRing []string, weight float64) {
 	for index, coins := range t.Data.RunCoins {
 		randomIndex := tools.SHA256int(fmt.Sprint(coins)) % len(coins)
 		selectedRing = append(selectedRing, coins[randomIndex])
@@ -80,6 +97,10 @@ func (t *Trader) selectRing() (selectedRing []string, weight float64) {
 	for i := 0; i < len(selectedRing); i++ {
 		coin := t.Data.Coins[selectedRing[i]]
 		weight += coin.Amount
+		if i > 0 {
+			ringMask += "-"
+		}
+		ringMask += coin.ID
 
 		coin.Prev = selectedRing[(i-1+len(selectedRing))%len(selectedRing)]
 		coin.Next = selectedRing[(i+1)%len(selectedRing)]
@@ -88,5 +109,5 @@ func (t *Trader) selectRing() (selectedRing []string, weight float64) {
 		t.Data.Coins[selectedRing[i]] = coin
 	}
 
-	return selectedRing, weight
+	return
 }
