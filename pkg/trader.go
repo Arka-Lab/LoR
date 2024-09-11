01e44aa93ee1d60fd4650e6c2312c448ed151b0c
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-11 14:34:32 +0330

    SHA3_256 -> SHA256

--- pkg/trader.go
+++ pkg/trader.go
@@ -14,7 +14,7 @@ type Trader struct {
 
 func (trader *Trader) CreateCoin(amount float64, coinType uint) *CoinTable {
 	return &CoinTable{
-		ID:       tools.SHA3_256(trader.ID + "-" + strconv.Itoa(int(coinType))),
+		ID:       tools.SHA256(trader.ID + "-" + strconv.Itoa(int(coinType))),
 		Amount:   amount,
 		Status:   Run,
 		Type:     coinType,
--- tools/sha.go
+++ tools/sha.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func SHA3_256(data string) string {
+func SHA256(data string) string {
 	h := sha3.New256()
 	h.Write([]byte(data))
 	return hex.EncodeToString(h.Sum(nil))
