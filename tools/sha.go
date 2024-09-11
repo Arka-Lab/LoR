989c3733c3fcd3355f5582de945bcade1638f87d
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-11 18:08:56 +0330

    implementing other sha256 output functions

--- tools/sha.go
+++ tools/sha.go
@@ -6,8 +6,28 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func SHA256(data string) string {
+func SHA256(data string) []byte {
 	h := sha3.New256()
 	h.Write([]byte(data))
-	return hex.EncodeToString(h.Sum(nil))
+	return h.Sum(nil)
+}
+
+func SHA256str(data string) string {
+	return hex.EncodeToString(SHA256(data))
+}
+
+func SHA256arr(data string) [8]int {
+	var result [8]int
+	for index, c := range SHA256(data) {
+		result[index/8] ^= int(c) << (index % 8)
+	}
+	return result
+}
+
+func SHA256int(data string) int {
+	var result int
+	for _, c := range SHA256arr(data) {
+		result ^= c
+	}
+	return result
 }
