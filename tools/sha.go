0d6f975ed0a9a62643870dfa77cbee4bb91235ad
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-14 16:12:00 +0330

    change SHA functions input to an interface instead of a string

--- tools/sha.go
+++ tools/sha.go
@@ -2,31 +2,32 @@ package tools
 
 import (
 	"encoding/hex"
+	"fmt"
 
 	"golang.org/x/crypto/sha3"
 )
 
-func SHA256(data string) []byte {
+func SHA256(data interface{}) []byte {
 	h := sha3.New256()
-	h.Write([]byte(data))
+	h.Write([]byte(fmt.Sprint(data)))
 	return h.Sum(nil)
 }
 
-func SHA256str(data string) string {
+func SHA256Str(data interface{}) string {
 	return hex.EncodeToString(SHA256(data))
 }
 
-func SHA256arr(data string) [8]int {
-	var result [8]int
+func SHA256Arr(data interface{}) []int {
+	result := make([]int, 8)
 	for index, c := range SHA256(data) {
-		result[index/8] ^= int(c) << (index % 8)
+		result[index/4] ^= int(c) << (index % 4 << 3)
 	}
 	return result
 }
 
-func SHA256int(data string) int {
+func SHA256Int(data interface{}) int {
 	var result int
-	for _, c := range SHA256arr(data) {
+	for _, c := range SHA256Arr(data) {
 		result ^= c
 	}
 	return result
