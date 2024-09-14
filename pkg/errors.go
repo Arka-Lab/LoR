cc276bd4faef025977fd8c49a3d941b8cca4bb09
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-14 16:10:51 +0330

    add new errors

--- pkg/errors.go
+++ pkg/errors.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrTraderAlreadyExist = errors.New("trader already exist")
 	ErrCoinAlreadyExist   = errors.New("coin already exist")
+	ErrInvalidCoinType    = errors.New("invalid coin type")
+	ErrRingAlreadyExist   = errors.New("ring already exist")
 )
