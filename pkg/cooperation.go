935cfc93c4ff0216accd1352db620e7a8d1f89d0
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-14 16:10:24 +0330

    add members field to cooperation ring

--- pkg/cooperation.go
+++ pkg/cooperation.go
@@ -1,15 +1,20 @@
 package pkg
 
 const (
-	RoundsCount = 10
+	RoundsCount     = 10
+	FractalMin      = 500
+	FractalMax      = 2000
+	VerificationMin = 20
+	VerificationMax = 50
 )
 
 type CooperationTable struct {
-	ID          string  `json:"id"`
-	MemberCount uint    `json:"member_count"`
-	Weight      float64 `json:"weight"`
-	Next        string  `json:"next"`
-	Prev        string  `json:"prev"`
-	Investor    string  `json:"investor"`
-	Rounds      uint    `json:"rounds"`
+	ID          string   `json:"id"`
+	MemberCount uint     `json:"member_count"`
+	Weight      float64  `json:"weight"`
+	Next        string   `json:"next"`
+	Prev        string   `json:"prev"`
+	Investor    string   `json:"investor"`
+	Rounds      uint     `json:"rounds"`
+	Members     []string `json:"-"`
 }
