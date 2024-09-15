af1b7d0c5fed4664efc878e32917c0a8561c5e41
Arshia Dadras <33801200+ArshiaDadras@users.noreply.github.com>
2024-09-15 21:12:04 +0330

    add RSA key functions

--- tools/random.go
+++ tools/random.go
@@ -1,5 +1,12 @@
 package tools
 
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+)
+
 func RandomSet(data []string, k int) ([]string, []string) {
 	arr := make([]string, len(data))
 	rnd := make([]int, 0)
@@ -13,4 +20,37 @@ func RandomSet(data []string, k int) ([]string, []string) {
 		arr[i], arr[index], rnd = arr[index], arr[i], rnd[1:]
 	}
 	return arr[:k], arr[k:]
+}
+
+func GeneratePrivateKey(size int) (*rsa.PrivateKey, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, size)
+	if err != nil {
+		return nil, err
+	}
+	if err := privateKey.Validate(); err != nil {
+		return nil, err
+	}
+	return privateKey, nil
+}
+
+func SignWithPrivateKey(data []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
+	hashed := sha256.Sum256(data)
+	return rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, hashed[:], nil)
+}
+
+func VerifyWithPublicKey(data []byte, signature []byte, publicKey *rsa.PublicKey) error {
+	hashed := sha256.Sum256(data)
+	return rsa.VerifyPSS(publicKey, crypto.SHA256, hashed[:], signature, nil)
+}
+
+func SignWithPrivateKeyStr(data string, privateKey *rsa.PrivateKey) (string, error) {
+	signature, err := SignWithPrivateKey([]byte(data), privateKey)
+	if err != nil {
+		return "", err
+	}
+	return string(signature), nil
+}
+
+func VerifyWithPublicKeyStr(data string, signature string, publicKey *rsa.PublicKey) error {
+	return VerifyWithPublicKey([]byte(data), []byte(signature), publicKey)
 }
