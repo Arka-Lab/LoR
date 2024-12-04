fb3d0c41d403dea692b0a4b6dfba8571fe20009e
Arshia Dadras <Arshia.Dadras@snapp.cab>
2024-12-04 17:46:17 +0330

    add feature to load system model from file

--- cmd/main.go
+++ cmd/main.go
@@ -9,52 +9,64 @@ import (
 	"github.com/Arka-Lab/LoR/pkg"
 )
 
-func ParseFlags() (int, time.Duration, int, int, int, string) {
+func ParseFlags() (int, time.Duration, int, int, int, string, string) {
 	typesPtr := flag.Int("type", 3, "number of coin types")
 	runTimePtr := flag.Int("time", 60, "run time in seconds")
 	tradersPtr := flag.Int("trader", 100, "number of traders")
 	randomsPtr := flag.Int("random", 0, "number of random traders")
 	badsPtr := flag.Int("bad", 0, "number of bad traders")
-	filepathPtr := flag.String("file", "system.json", "file path to save system")
+	saveTohPtr := flag.String("save-to", "system.json", "file path to save system")
+	loadFromhPtr := flag.String("load-from", "", "file path to load system")
 	flag.Parse()
 
 	numTypes, numTraders := *typesPtr, *tradersPtr
 	runTime := time.Duration(*runTimePtr) * time.Second
 	numRandoms, numBads := *randomsPtr, *badsPtr
-	filePath := *filepathPtr
+	saveTo, loadFrom := *saveTohPtr, *loadFromhPtr
 
-	return numTypes, runTime, numTraders, numRandoms, numBads, filePath
+	return numTypes, runTime, numTraders, numRandoms, numBads, saveTo, loadFrom
 }
 
 func main() {
 	logger := log.Default()
-	finish := make(chan bool, 1)
-	system := internal.NewSystem()
+	var system *internal.System
+	numTypes, runTime, numTraders, numRandoms, numBads, saveTo, loadFrom := ParseFlags()
 
-	numTypes, runTime, numTraders, numRandoms, numBads, filePath := ParseFlags()
+	if loadFrom == "" {
+		finish := make(chan bool, 1)
+		system = internal.NewSystem()
 
-	logger.Printf("Starting simulation with %d types (alpha = %.2f%%)...\n", numTypes, pkg.BadBehavior*100)
-	system.Init(numTraders, numRandoms, numBads, uint(numTypes))
-	logger.Println("Simulation initialized!")
+		logger.Printf("Starting simulation with %d types (alpha = %.2f%%)...\n", numTypes, pkg.BadBehavior*100)
+		system.Init(numTraders, numRandoms, numBads, uint(numTypes))
+		logger.Println("Simulation initialized!")
 
-	logger.Println("Starting simulation...")
-	done := make(chan bool, 1)
-	go func() {
-		system.Start(finish)
-		done <- true
-	}()
-	logger.Println("Simulation started!")
+		logger.Println("Starting simulation...")
+		done := make(chan bool, 1)
+		go func() {
+			system.Start(finish)
+			done <- true
+		}()
+		logger.Println("Simulation started!")
 
-	logger.Printf("Waiting for %s...\n", runTime)
-	time.Sleep(runTime)
-	finish <- true
-	<-done
-	logger.Println("Simulation stopped!")
+		logger.Printf("Waiting for %s...\n", runTime)
+		time.Sleep(runTime)
+		finish <- true
+		<-done
+		logger.Println("Simulation stopped!")
 
-	if err := system.Save(filePath); err != nil {
-		logger.Fatalf("Error saving system: %v\n", err)
+		if err := system.Save(saveTo); err != nil {
+			logger.Fatalf("Error saving system: %v\n", err)
+		}
+		logger.Printf("System saved to %s\n", saveTo)
+	} else {
+		s, err := internal.Load(loadFrom)
+		if err != nil {
+			logger.Fatalf("Error loading system: %v\n", err)
+		}
+
+		system = s
+		logger.Printf("Simulation loaded from %s\n", loadFrom)
 	}
-	logger.Printf("System saved to %s\n", filePath)
 
 	internal.AnalyzeSystem(system)
 }
